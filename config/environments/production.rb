e909efb0561bce02959e637cf97af5224ca68363
philipfweiss <weissp68@stanford.edu>
2016-08-03 16:16:13 -0700

    fix production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,11 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
