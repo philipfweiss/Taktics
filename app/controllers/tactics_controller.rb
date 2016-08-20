4955cbbb5fa5ee6395874a1c08d865047cf707ac
philipfweiss <weissp68@stanford.edu>
2016-08-19 21:04:28 -0700

    fix issue?

--- app/controllers/tactics_controller.rb
+++ app/controllers/tactics_controller.rb
@@ -3,6 +3,12 @@ class TacticsController < ApplicationController
     @user = current_user
     @tactic = Tactic.find(1)
     @moves = @tactic.options
+    @moves[:one] ||= "Nothing!"
+    @moves[:two] ||= "Nothing!"
+    @moves[:three] ||= "Nothing!"
+    @moves[:four] ||= "Nothing!"
+    @moves[:correct] ||= "Nothing!"
+
   end
 
   def index
