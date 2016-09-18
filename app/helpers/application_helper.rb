83dc15f50625cf079e547d654bf750bebea06c0b
philipfweiss <weissp68@stanford.edu>
2016-09-18 14:57:41 -0700

    update title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
    def full_title(page_title='')
-     base_title = "Application Base"
+     base_title = "Taktics.net"
      if page_title.empty?
        base_title
      else
